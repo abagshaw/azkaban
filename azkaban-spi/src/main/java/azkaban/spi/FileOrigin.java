2ee6dfbd1cc88d519bd8f6b2d3f0c55e0371ccbc
abagshaw <abagshaw@linkedin.com>
2019-10-21 21:54:22 -0700

    DownloadOrigin -> FileOrigin

--- azkaban-spi/src/main/java/azkaban/spi/FileOrigin.java
+++ azkaban-spi/src/main/java/azkaban/spi/FileOrigin.java
@@ -2,6 +2,6 @@ package azkaban.spi;
 
 // STOARGE = download the file from Storage
 // REMOTE = download the file from a remote, external URL
-public enum DownloadOrigin {
+public enum FileOrigin {
   STORAGE, REMOTE
 }
