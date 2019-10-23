2e97ce282b185826eb699f1178f2463ecff06042
abagshaw <abagshaw@linkedin.com>
2019-10-23 16:19:59 -0700

    fix primary key column order

--- azkaban-db/src/main/sql/create.validated_dependencies.sql
+++ azkaban-db/src/main/sql/create.validated_dependencies.sql
@@ -3,5 +3,5 @@ CREATE TABLE validated_dependencies (
   file_sha1         VARCHAR(40),
   validation_key    VARCHAR(40),
   validation_status INT,
-  PRIMARY KEY (file_sha1, file_name, validation_key)
+  PRIMARY KEY (file_name, file_sha1, validation_key)
 );
